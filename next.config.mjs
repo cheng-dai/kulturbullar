2d911c2dc8359e804dcc049e45d99e8451a604df
chengdai <chengdai.dev@gmail.com>
2024-09-11 16:57:04 +0200

    redirect to new website

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: "/",
+        destination: "https://www.kulturbullar.com/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
