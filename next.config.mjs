04ede55f9a74761867483375ffbb8240ce5bed8f
chengdai <chengdai.dev@gmail.com>
2024-09-11 17:13:39 +0200

    changed redirect in config to false

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
       {
         source: "/",
         destination: "https://www.kulturbullar.com/",
-        permanent: true,
+        permanent: false,
       },
     ];
   },
